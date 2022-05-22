9df90d64be95f2168d918ddcfeeb9e2be00664d9
pedrochunks35 <a26899@sa-miranda.net>
2022-05-22 11:25:35 +0100

    Vamos

--- readme.txt
+++ readme.txt
@@ -1,3 +1,7 @@
 Truffle is meant to made the propper connection to an certain blockchain.
 We build contracts in the contracts dir and we export them in migrations with the name of the contract and not the contract file.
 test dir will be our truffle unit test , be free to check out the examples there.
+.gitgnore para ignorar files da nossa dir , neste caso .env
+no .env pusemos as nossas private keys de 2 contas criadas no metamask e tambem a frase secreta..
+depois instalamos um modulo npm dotenv e importamos o mesmo e atraves dessa livraria fomos buscar as nossas chaves secretas
+depois disso colocamos uma configuracao nova de rede para uma test net atraves da ethereum API senao tinhamos que ser nos mesmos a configurar um node.
--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,12 @@
+require('dotenv').config()
+//waler provider mechanism to sign transactions
+const HDwalletProvider = require("@truffle/hdwallet-provider"); 
+//storage private keys
+const private_keys = [
+process.env.PRIVATE_KEY0,
+process.env.PRIVATE_KEY1,
+]
+
 module.exports={
 
   networks:{
@@ -6,6 +15,19 @@ module.exports={
       host: "127.0.0.1",
       port: "8545",
       network_id: "*" //ANY NETWORK ID
+    },
+    //eth testnet
+    rinkeby:{
+      provider: () => new HDWalletProvider({
+      private_keys: private_keys,
+      providerorUrl: 'https://rinkeby.infura.io/v3/ffa0ca21d5a543e791026bf4e6ae6dc9',
+      numberOfAddress: 2
+      }),
+      network_id: 42,
+      gas: 5500000,
+      confirmations : 2,
+      timeoutBlocks : 200, // # of blocks before a deployment times out (minimum/default : 50)
+      skipDryRun: true
     }
   },
   solc:{
